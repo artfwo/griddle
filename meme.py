c698ca57a1ca7c2fe111263243f3a30dff35d774
Artem Popov <artfwo@gmail.com>
2011-05-30 16:10:04 +0700

    delete old meme

--- meme.py
+++ meme.py
@@ -3,87 +3,113 @@
 
 import gtk, gobject
 
-import liblo
-from liblo import *
+from OSC import OSCClient, OSCServer, OSCMessage
 import select, pybonjour
 REGTYPE = "_monome-osc._udp"
 NAME_FORMAT = "meme-%d"
 
 def bitarray(b):
-    #return [b>>7&1, b>>6&1, b>>5&1, b>>4&1, b>>3&1, b>>2&1, b>>1&1, b&1]
     return [b&1, b>>1&1, b>>2&1, b>>3&1, b>>4&1, b>>5&1, b>>6&1, b>>7&1]
 
-class Justosc(Server):
+def quick_message(host, port, path, *args):
+    msg = OSCMessage(path)
+    [msg.append(d) for d in args]
+    client = OSCClient()
+	client.sendto(msg, (host, port), timeout=0)
+
+class Meme(OSCServer):
     def __init__(self, port, gui):
-        Server.__init__(self, port=port)
+        OSCServer.__init__(self, ('localhost', port))
         self.app_port = 8000
         self.app_host = "localhost"
         self.prefix = "/monome"
         self.rotation = 0 # FIXME
         self.gui = gui
+        
+        self.register_syscallbacks()
         self.register_callbacks(self.prefix)
     
-    @make_method('/sys/port', 'i')
-    def sys_port(self, path, args):
-        port, = args
-        self.app_port = port
+    def sys_port(self, addr, tags, data, client_address):
+        quick_message(self.app_host, self.app_port, "/sys/port", self.app_port)
+        self.app_port = data[0]
+        quick_message(self.app_host, self.app_port, "/sys/port", self.app_port)
     
-    @make_method('/sys/host', 's')
-    def sys_host(self, path, args):
-        host, = args
-        self.app_host = host
+    def sys_host(self, addr, tags, data, client_address):
+        quick_message(self.app_host, self.app_port, "/sys/host", self.app_host)
+        self.app_host = data[0]
+        quick_message(self.app_host, self.app_port, "/sys/host", self.app_host)
     
-    @make_method('/sys/prefix', 's')
-    def sys_prefix(self, path, args):
-        prefix, = args
+    def sys_prefix(self, addr, tags, data, client_address):
+        prefix = data[0]
         self.unregister_callbacks(self.prefix)
         self.prefix = "/%s" % prefix.strip("/")
         self.register_callbacks(self.prefix)
+        quick_message(self.app_host, self.app_port, "/sys/prefix", self.prefix)
     
-    @make_method('/sys/info', None)
-    def sys_info(self, path, args):
-        if len(args) == 2:
-            host, port = args
-        elif len(args) == 1:
-            host, port = self.app_host, args[0]
-        elif len(args) == 0:
+    def sys_info(self, addr, tags, data, client_address):
+        if len(data) == 2:
+            host, port = data
+        elif len(data) == 1:
+            host, port = self.app_host, data[0]
+        elif len(data) == 0:
             host, port = self.app_host, self.app_port
         else:
             return
-        target = liblo.Address(host, port)
-        liblo.send(target, "/sys/port", port)
-        liblo.send(target, "/sys/host", host)
-        liblo.send(target, "/sys/id", "meme")
-        liblo.send(target, "/sys/prefix", self.prefix)
+        
+        quick_message(host, port, "/sys/port", port)
+        quick_message(host, port, "/sys/host", host)
+        quick_message(host, port, "/sys/id", "meme")
+        quick_message(host, port, "/sys/prefix", self.prefix)
+        quick_message(host, port, "/sys/size", self.gui.xsize, self.gui.ysize)
     
     def unregister_callbacks(self, prefix):
-        self.del_method("%s/grid/led/set" % prefix, "iii")
-        self.del_method("%s/grid/led/map" % prefix, "iiiiiiiiii")
-        self.del_method("%s/grid/led/row" % prefix, None)
-        self.del_method("%s/grid/led/col" % prefix, None)
+        self.delMsgHandler("%s/grid/led/set" % prefix)
+        self.delMsgHandler("%s/grid/led/map" % prefix)
+        self.delMsgHandler("%s/grid/led/row" % prefix)
+        self.delMsgHandler("%s/grid/led/col" % prefix)
+        self.delMsgHandler("%s/grid/led/all" % prefix)
     
     def register_callbacks(self, prefix):
-        self.add_method("%s/grid/led/set" % prefix, "iii", self.grid_led_set)
-        self.add_method("%s/grid/led/map" % prefix, "iiiiiiiiii", self.grid_led_map)
-        self.add_method("%s/grid/led/row" % prefix, None, self.grid_led_row)
-        self.add_method("%s/grid/led/col" % prefix, None, self.grid_led_col)
+        self.addMsgHandler("%s/grid/led/set" % prefix, self.grid_led_set)
+        self.addMsgHandler("%s/grid/led/map" % prefix, self.grid_led_map)
+        self.addMsgHandler("%s/grid/led/row" % prefix, self.grid_led_row)
+        self.addMsgHandler("%s/grid/led/col" % prefix, self.grid_led_col)
+        self.addMsgHandler("%s/grid/led/all" % prefix, self.grid_led_all)
 
-    def grid_led_set(self, path, args):
-        x, y, s = args
+    def register_syscallbacks(self):
+        self.addMsgHandler("/sys/port", self.sys_port)
+        self.addMsgHandler("/sys/host", self.sys_host)
+        self.addMsgHandler("/sys/prefix", self.sys_prefix)
+        self.addMsgHandler("/sys/info", self.sys_info)
+
+    def grid_led_set(self, addr, tags, data, client_address):
+        print "led set", addr, data
+        x, y, s = data
         if s == 1:
             self.gui.light_button(x, y)
         else:
             self.gui.unlight_button(x, y)
     
-    def grid_led_map(self, path, args):
-        x_offset = args[0]
-        y_offset = args[1]
-        s = args[2:]
+    def grid_led_map(self, addr, tags, data, client_address):
+        x_offset = data[0]
+        y_offset = data[1]
+        s = data[2:]
         for i in range(len(s)):
-            self.grid_led_row(None, [x_offset, y_offset+i, s[i]])
+            self.grid_led_row(None, None, [x_offset, y_offset+i, s[i]], None) # FIXME
     
     # FIXME: need len(args) check
-    def grid_led_row(self, path, args):
+    def grid_led_row(self, addr, tags, data, client_address):
+        x_offset = data[0]
+        y_offset = data[1]
+        s = data[2:]
+        bits = reduce(lambda x, y: x+y, [bitarray(b) for b in s], [])
+        
+        for b in range(len(bits)):
+            bit = int(bits[b])
+            self.grid_led_set(None, None, [x_offset+b, y_offset, bit], None) # FIXME
+    
+    # FIXME: need len(args) check
+    def grid_led_col(self, addr, tags, data, client_address):
         x_offset = args[0]
         y_offset = args[1]
         s = args[2:]
@@ -91,54 +117,29 @@ class Justosc(Server):
         
         for b in range(len(bits)):
             bit = int(bits[b])
-            self.grid_led_set(None, [x_offset+b, y_offset, bit])
+            self.grid_led_set(None, None, [x_offset, y_offset+b, bit], None) # FIXME
     
-    # FIXME: need len(args) check
-    def grid_led_col(self, path, args):
-        x_offset = args[0]
-        y_offset = args[1]
-        s = args[2:]
-        bits = reduce(lambda x, y: x+y, [bitarray(b) for b in s], [])
-        
-        for b in range(len(bits)):
-            bit = int(bits[b])
-            self.grid_led_set(None, [x_offset, y_offset+b, bit])
+    def grid_led_all(self, addr, tags, data, client_address):
+        s = data[0]
+        self.gui.led_all(s)
     
     def grid_key(self, x, y, s):
-        target = liblo.Address(self.app_host, self.app_port)
-        liblo.send(target, "%s/grid/key" % self.prefix, x, y, s)
-
-class Meme(Server):
-    def __init__(self, port, gui):
-        self.server = Justosc(port, gui)
-        #self.gui = gui
-        self.sdRef = pybonjour.DNSServiceRegister(name = NAME_FORMAT % port,
-             regtype = REGTYPE,
-             port = port,
-             callBack = self.register_callback)
-    
-    def register_callback(self, sdRef, flags, errorCode, name, regtype, domain):
-        if errorCode == pybonjour.kDNSServiceErr_NoError:
-            print 'Registered service:', name, regtype, domain
-            #self.waffle = Waffle(20000)
-            #self.waffle.start()
-            pass
-    
-    def poll(self):
-        ready = select.select([self.sdRef, self.server], [], [], 0)
-        #print ready
-        if self.sdRef in ready[0]:
-            pybonjour.DNSServiceProcessResult(self.sdRef)
-        if self.server in ready[0]:
-            self.server.recv(0)
-    
-    def close(self):
-        self.sdRef.close()
+        msg = OSCMessage("%s/grid/key" % self.prefix)
+        msg.append(x)
+        msg.append(y)
+        msg.append(s)
+    	self.client.sendto(msg, (self.app_host, self.app_port), timeout=0)
 
 class MemeGui(gtk.Window):
     def __init__(self, port, xsize, ysize):
         gtk.Window.__init__(self)
-        self.s = Meme(port, self)
+
+        self.meme = Meme(port, self)
+        self.meme_service = pybonjour.DNSServiceRegister(name = NAME_FORMAT % port,
+             regtype = REGTYPE,
+             port = port,
+             callBack = None)
+        
         self.set_title(NAME_FORMAT % port)
         
         self.xsize = xsize
@@ -159,17 +160,27 @@ class MemeGui(gtk.Window):
                 b.connect("clicked", self.button_clicked, x, y)
                 self.unlight_button(x, y)
         
-        gobject.timeout_add(100, self.idle)
+        gobject.timeout_add(10, self.idle)
     
     def idle(self):
-        self.s.poll()
-        #import time
-        #time.sleep(0.001)
+        ready = select.select([self.meme_service, self.meme], [], [], 0)
+        if self.meme_service in ready[0]:
+            pybonjour.DNSServiceProcessResult(self.meme_service)
+        if self.meme in ready[0]:
+            self.meme.handle_request()
         return True
     
     def button_clicked(self, b, x, y):
-        print "clicked", b, x, y
-        self.s.server.grid_key(x, y, 1)
+        self.meme.grid_key(x, y, 1)
+    
+    def led_all(self, s):
+        if s == 0:
+            func = self.unlight_button
+        else:
+            func = self.light_button
+        for x in range(self.xsize):
+            for y in range(self.ysize):
+                func(x, y)
     
     def light_button(self, x, y):
         if not x in range(self.xsize) or not y in range(self.ysize): return
@@ -186,12 +197,13 @@ class MemeGui(gtk.Window):
         b.modify_bg(gtk.STATE_ACTIVE, gtk.gdk.Color("#ffffff"))
     
     def do_delete_event(self, *args):
-        self.s.close()
+        self.meme.close()
+        self.meme_service.close()
         gtk.main_quit()
 
 gobject.type_register(MemeGui)
 
-w = MemeGui(18081, 8, 8)
+w = MemeGui(8081, 16, 16)
 w.show_all()
 gtk.main()
 
