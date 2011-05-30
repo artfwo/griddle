bfb914c305f882976b9c61ffc438b58c7ccff23f
Artem Popov <artfwo@gmail.com>
2011-05-30 12:34:24 +0700

    listen to all interfaces by default.

--- griddle.py
+++ griddle.py
@@ -51,7 +51,7 @@ class Waffle:
 # TODO: /sys/connect
 class Monome(OSCServer, Waffle):
     def __init__(self, id, host, port):
-        OSCServer.__init__(self, ('localhost', 0))
+        OSCServer.__init__(self, ('', 0))
         self.id = id
         self.focused = False
         self.target_host = host
@@ -98,7 +98,7 @@ class Monome(OSCServer, Waffle):
 
 class VirtualMonome(OSCServer, Waffle):
     def __init__(self, id, xsize, ysize, port=0):
-        OSCServer.__init__(self, ('localhost', port))
+        OSCServer.__init__(self, ('', port))
         self.id = id
         self.xsize = xsize
         self.ysize = ysize
